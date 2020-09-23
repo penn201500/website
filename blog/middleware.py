f5b4134aa0b3bae336f477b97337ea1abfd49709
penn <penn201500@gmail.com>
2020-09-23 16:55:46 +0800

    bugfix: self defined middleware bugfix

--- blog/middleware.py
+++ blog/middleware.py
@@ -1,6 +1,4 @@
 # -*- coding:UTF-8 -*-
-import json
-
 from django.shortcuts import render
 from rest_framework import status
 from django.utils.deprecation import MiddlewareMixin
@@ -9,14 +7,16 @@ from django.utils.deprecation import MiddlewareMixin
 class ApiMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if response.status_code == status.HTTP_403_FORBIDDEN:
-            return render(request, 'blog/403.html', locals())
+            return render(response, 'blog/403.html', locals())
 
         if response.status_code == status.HTTP_400_BAD_REQUEST:
-            return render(request, 'blog/400.html', locals())
+            return render(response, 'blog/400.html', locals())
 
         if response.status_code == status.HTTP_404_NOT_FOUND:
-            return render(request, 'blog/404.html', locals())
+            return render(response, 'blog/404.html', locals())
 
         if response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR:
-            return render(request, 'blog/500.html', locals())
+            return render(response, 'blog/500.html', locals())
+
+        return response
 
