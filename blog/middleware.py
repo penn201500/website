6e994f5236162cb90c60a4d79f86c07913c42f92
penn <penn201500@gmail.com>
2020-09-23 16:43:28 +0800

    add response error code process html

--- blog/middleware.py
+++ blog/middleware.py
@@ -1,28 +1,22 @@
 # -*- coding:UTF-8 -*-
 import json
+
+from django.shortcuts import render
 from rest_framework import status
 from django.utils.deprecation import MiddlewareMixin
 
 
 class ApiMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        message = ""
-        code = 1
-
         if response.status_code == status.HTTP_403_FORBIDDEN:
-            code = 4
-            message = "您没有执行此操作的权限，请联系管理员获取相应的权限。"
+            return render(request, 'blog/403.html', locals())
 
-        if response.status_code == status.HTTP_400_BAD_REQUEST and not message:
-            message = "缺少必要的请求参数或参数错误"
-            code = 2
+        if response.status_code == status.HTTP_400_BAD_REQUEST:
+            return render(request, 'blog/400.html', locals())
 
         if response.status_code == status.HTTP_404_NOT_FOUND:
-            message = "Page Not Found"
-            code = 2
+            return render(request, 'blog/404.html', locals())
 
-        response.content = json.dumps({
-            "code": code,
-            "message": message,
-        })
-        return response
+        if response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR:
+            return render(request, 'blog/500.html', locals())
+
