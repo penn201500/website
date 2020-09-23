af1109d94b75668d0290cb044983db364124c9de
penn <penn201500@gmail.com>
2020-09-23 16:27:17 +0800

    add middleware to process 404,403,400

---
+++ blog/middleware.py
@@ -0,0 +1,29 @@
+# -*- coding:UTF-8 -*-
+import json
+from rest_framework import status
+from django.utils.deprecation import MiddlewareMixin
+
+
+class ApiMiddleware(MiddlewareMixin):
+    @staticmethod
+    def process_response(response):
+        message = ""
+        code = 1
+
+        if response.status_code == status.HTTP_403_FORBIDDEN:
+            code = 4
+            message = "您没有执行此操作的权限，请联系管理员获取相应的权限。"
+
+        if response.status_code == status.HTTP_400_BAD_REQUEST and not message:
+            message = "缺少必要的请求参数或参数错误"
+            code = 2
+
+        if response.status_code == status.HTTP_404_NOT_FOUND:
+            message = "Page Not Found"
+            code = 2
+
+        response.content = json.dumps({
+            "code": code,
+            "message": message,
+        })
+        return response
